f969f062f03522ca7a154aba7138a9f9a4ef2026
Shamil Umaev <shamil.umaev@hotmail.com>
2023-09-27 23:09:19 +0200

    Function to shorten the URL using a shrtcode API

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Hamburger Menu for mobile version
 const hamburgerBtn = document.querySelector('.hamburger-icon');
+const urlInput = document.querySelector('input[type="text"]');
+const form = document.querySelector('form');
 
+// Show/Hide Hamburger Menu for mobile version
 const showHideMobileMenu = () => {
     const navCtr = document.querySelector('.mobile-nav');
     
@@ -13,4 +15,20 @@ const showHideMobileMenu = () => {
     }
 } 
 
+// Shorten the URL
+const shortenUrl = async (urlInput) => {
+    const API_URL = `https://api.shrtco.de/v2/shorten?url=${urlInput.value}`
+    const response = await fetch(API_URL);
+    const { result } = await response.json();
+    console.log(result);
+    urlInput.value = '';
+}
+
+// Submit the URL
+const submitUrl = (e) => {
+    e.preventDefault();
+    shortenUrl(urlInput);
+}
+
 hamburgerBtn.addEventListener('click', showHideMobileMenu);
+form.addEventListener('submit', submitUrl);
